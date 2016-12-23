c8e4b26a7711ace3391a9344be44161b26e7e6e5
owen <ow.diehl@gmail.com>
2016-12-23 15:11:15 -0500

    updates gradient clipping, adds fn to load mnist dataset for testing on a known set

--- single-threaded/train.py
+++ single-threaded/train.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from model import simple, inputs
 import matplotlib.pyplot as plt
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
 flags = tf.app.flags
 flags.DEFINE_string('train_dir', '/tmp/data',
@@ -18,6 +19,13 @@ def view_image(image_buffer):
   plt.imshow(image_buffer)
   plt.show()  
 
+def mnist_inputs():
+  mnist = read_data_sets('/tmp/mnist_data', one_hot=True)
+  images, labels = mnist.train.next_batch(batch_size)
+  images = tf.reshape(images, [-1,28,28,1])
+  labels = tf.pack(labels)
+  return images, labels
+ 
 def train(train_dir, batch_size, num_batches, log_dir):
   images, labels = inputs(train_dir,
               batch_size,
@@ -27,11 +35,11 @@ def train(train_dir, batch_size, num_batches, log_dir):
 
   predictions = simple(images)
   slim.losses.softmax_cross_entropy(predictions, labels)
-  total_loss = tf.clip_by_value(slim.losses.get_total_loss(), 1e-10, 1.0)
+  total_loss = tf.clip_by_value(slim.losses.get_total_loss(), 1e-10, 1000000.0)
   tf.scalar_summary('loss', total_loss)
 
   #optimizer = tf.train.RMSPropOptimizer(0.001, 0.9)
-  optimizer = tf.train.GradientDescentOptimizer(0.005)
+  optimizer = tf.train.GradientDescentOptimizer(0.001)
   train_op = slim.learning.create_train_op(total_loss, optimizer, summarize_gradients=True)
 
   slim.learning.train(train_op, log_dir, save_summaries_secs=20)
